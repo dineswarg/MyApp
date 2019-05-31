7f3abfd379b620d7a5a77fa7f814ff6f22580b4a
dineswarg <dineswarg5@gmail.com>
2019-05-30 22:34:52 -0500

    initial push to git

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
+
+
+
+
+
