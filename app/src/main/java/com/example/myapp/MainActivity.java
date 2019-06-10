243d8a8c30792e6bbb87703dd647d751cf3ffe76
dineswarg <dineswarg5@gmail.com>
2019-06-10 11:19:15 -0500

    new commit

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Log.e("main activity","................");
+
+        Log.e(".............",".............");
     }
 }
 
